1bde66f044a32186d74ed21d871f388813c14a28
Yashwanth-13 <yashwanth.chilaka@gmail.com>
2023-06-24 14:04:50 +0530

    'test'

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/registration')
+def registration():
+    return render_template("registration.html")
