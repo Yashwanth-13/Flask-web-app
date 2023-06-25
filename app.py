f604abfaaf9276620e4daffeace472b1234a8bdf
Yashwanth-13 <yashwanth.chilaka@gmail.com>
2023-06-25 17:50:21 +0530

    Some side cases

--- app.py
+++ app.py
@@ -18,20 +18,35 @@ def login():
         course = request.form.get("course")
         gender  = request.form.get("Gender")
         phone = request.form.get("phone")
+
+        # Checking if the user entered valid data
         if len(phone) != 10:
             return "<p>Invalid Phone number!</p>"
         email = request.form.get("email")
         values = ["@", ".com"]
         if not (values[0] in email and values[1] in email):
             return "<p>Invalid Email!</p>"
+        
+        # Checking if the user already exists
         username = request.form.get("username")
+        existing_user = cursor.execute("SELECT username FROM credentials",).fetchall()
+        for tup in existing_user:
+            for value in tup:
+                if username == value:
+                    return "<p>Username already exists!</p>"
+
         password = request.form.get("password")
         reTypedPassword = request.form.get("re-typed")
+
+        # Checking if the passwords match
         if password != reTypedPassword:
             return "<p>Passwords do not match!</p>"
+        
+        # Adding the user to the database
         cursor.execute("INSERT INTO credentials (username, password) VALUES (?, ?)", (username, password,))
         cursor.execute("INSERT INTO data (data_id, first_name, last_name, gender, phone, mail, course) VALUES ((SELECT id FROM credentials WHERE username = ?), ?, ?, ?, ?, ?, ?)", (username, firstname, lastname, gender, phone, email, course,))
         db.commit()
+
         return render_template("login.html")
         
 
@@ -46,17 +61,13 @@ def home():
             the_names = cursor.execute("SELECT first_name, last_name FROM data WHERE data_id = ?", (user_id,),).fetchall()
             first_name = the_names[0][0]
             last_name = the_names[0][1]
-            user_db = sqlite3.connect("databases/current_user.db", check_same_thread=False)
-            user_cursor = user_db.cursor()
-            user_cursor.execute("INSERT INTO current_user (first_name, last_name) VALUES (?, ?)", (first_name, last_name,))
-            user_db.commit()
+            cursor.execute("INSERT INTO current_user (first_name, last_name) VALUES (?, ?)", (first_name, last_name,))
+            db.commit()
             return render_template("home.html", first_name=first_name, last_name=last_name)
         else:
             return render_template("invalid_login.html")
     else:
-        user_db = sqlite3.connect("databases/current_user.db", check_same_thread=False)
-        user_cursor = user_db.cursor()
-        names = user_cursor.execute("SELECT first_name, last_name FROM current_user").fetchall()
+        names = cursor.execute("SELECT first_name, last_name FROM current_user").fetchall()
         first_name = names[0][0]
         last_name = names[0][1]
         return render_template("home.html", first_name=first_name, last_name=last_name)
