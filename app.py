38247d7839033e08fa1cfb7aca93f1e478e8b494
Yashwanth-13 <yashwanth.chilaka@gmail.com>
2023-06-25 14:27:25 +0530

    regis

--- app.py
+++ app.py
@@ -10,13 +10,27 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        firstname = request.form.get("firstname")
+        lastname = request.form.get("lastname")
+        course = request.form.get("course")
+        gender  = request.form.get("gender")
+        phone = request.form.get("phone")
+        if len(phone) != 10:
+            return render_template("invalid_register.html", invalid=phone)
+        email = request.form.get("email")
+        values = ["@", ".com"]
+        if values not in email:
+            return render_template("invalid_register.html", invalid=email)
+        
 
 @app.route('/home', methods=["GET", "POST"])
 def home():
     if request.method == "POST":
-        user_name = request.form["username"]
-        pass_word = request.form["password"]
+        user_name = request.form.get("username")
+        pass_word = request.form.get("password")
         user_id = cursor.execute("SELECT id FROM credentials WHERE username = ? AND password = ?", (user_name, pass_word,),).fetchall()
         if user_id:
             first_name, last_name = cursor.execute("SELECT first_name, last_name FROM data WHERE data_id = ?", (user_id[0]),).fetchall()
